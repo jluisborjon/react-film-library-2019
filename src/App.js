39642ba902e1a0de9d9c3b34bb54c5b8dc0d8608
Jose Borjon <jluisborjon@gmail.com>
2019-04-10 00:26:29 -0400

    faves

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import FilmList from './FilmList';
+import FavesContext from './FavesContext';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+const App = () => {
+  const [faves, setFaves] = useState([]);
+
+  const onFaveToggle =(film) => {
+    const favesCopy = faves.slice();
+    const filmIndex = faves.indexOf(film);
+
+    if (faves.includes(film)) {
+      // remove it from faves
+      console.log(`Removing ${film.title} from faves`);
+      favesCopy.splice(filmIndex, 1);
+      setFaves(favesCopy);
+    } else{
+      // add it to faves
+      console.log(`Adding ${film.title} to faves`);
+      setFaves([...favesCopy, film]);
+    }
+  };
+
+  return (
+    <main className="film-library">
+      <FavesContext.Provider value={{faves, toggleFave: onFaveToggle}}>
+        <FilmList onFaveToggle={onFaveToggle} />
+
+        <section className="film-details">
+          <h1 className="section-title">DETAILS</h1>
+        </section>
+      </FavesContext.Provider>
+    </main>
+
+  );
 }
 
 export default App;
---
+++ src/Fave.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Fave = ({onFaveToggle}) => {
+  const handleClick = (event) => {
+    event.stopPropagation();
+    console.log(`Fave was clicked!`);
+    onFaveToggle();
+  }
+  return (
+    <button className={`film-row-fave add_to_queue`} onClick={handleClick}>
+      <i className="material-icons">add_to_queue</i>
+    </button>
+  );
+};
+
+export default Fave;
